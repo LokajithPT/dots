50989759b736ffa4e60e44529eea59743e95ab63
l0wkey <lowkey@wow.com>
2026-08-28 22:37:43 +0530

    Update configuration files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,11 @@
 require("config.lazy")
 vim.opt.number = true
 
+-- Use system clipboard for yank/paste
+vim.opt.clipboard = 'unnamedplus'
+-- Optional explicit mappings (normally works with unnamedplus)
+vim.keymap.set({ 'n','v' }, 'y', '"+y', { noremap = true, silent = true })
+vim.keymap.set({ 'n','v' }, 'Y', '"+Y', { noremap = true, silent = true })
+
 -- Toggle NvimTree with <C-n>
 vim.keymap.set('n','<C-n>',':NvimTreeToggle<CR>',{noremap=true,silent=true})
